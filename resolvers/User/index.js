7e5c9f457707b131bb9636ff108fec1107cc37e4
julio cesar rodriguez nieves <jcrodriguez@alioit.com>
2019-09-13 15:41:06 -0700

    added allUsersPaging

--- resolvers/User/index.js
+++ resolvers/User/index.js
@@ -1,11 +1,17 @@
 import { fakeDataBase } from '../../models';
-import { applyFilter } from '../../lib/helper';
+import { applyFilter, applyPaging } from '../../lib/helper';
 
 const resolversUser = {
   Query: {
     allUsers: () => fakeDataBase.user,
     userById: (_, user) => fakeDataBase.user.find(value => value.id === parseInt(user.id, 10)),
     findUsers: (_, user) => fakeDataBase.user.filter(applyFilter(JSON.parse(user.pattern))),
+    allUsersPaging: (_, user) => {
+      const { page, pageSize } = user;
+      const from = (page - 1) * pageSize;
+      const to = from + pageSize;
+      return fakeDataBase.user.filter(applyPaging(from, to));
+    },
   },
   User: {
     pets: user => fakeDataBase.pet.filter(pet => pet.userId === user.id),
